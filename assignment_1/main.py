6c780d35ecd7b56faa2be2faa18f222ba7bac031
Magnus Green <magnussg@uia.no>
2026-08-30 16:53:13 +0200

    assig 1

---
+++ assignment_1/main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+image = cv2.imread('iris-1.jpg',1)
+
+
+def print_image_information(image):
+    print("Height:", image.shape[0])
+    print("Width:", image.shape[1])
+    print("Channels:", image.shape[2])
+    print("Size:", image.size)
+    print("Data type:", image.dtype)
+
+print_image_information(image)
+
+
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cam_fps= int(cam.get(cv2.CAP_PROP_FPS))
+
+
+print("Frame width:", frame_width)
+print("Frame height:", frame_height)
+print("Camera FPS:", cam_fps)
+
+cam.release()
+
+
+
+with open("camere_outputs.txt", "w") as f:
+    f.write("fps: " + str(cam_fps) + "\n")
+    f.write("frame_height: " + str(frame_height) + "\n")
+    f.write("frame_width: " + str(frame_width))
